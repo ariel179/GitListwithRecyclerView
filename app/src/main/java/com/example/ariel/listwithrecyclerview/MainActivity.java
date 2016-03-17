c1889b3c686e6a005992b7b32bb02cd68893ac54
ariel179 <ariel179@gmail.com>
2016-03-17 11:36:38 +1100

    Initial commit

---
+++ app/src/main/java/com/example/ariel/listwithrecyclerview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ariel.listwithrecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<String> array = new ArrayList<>();
+        array.add("Apple");
+        array.add("Orange");
+        array.add("Berry");
+        array.add("Banana");
+        array.add("Mango");
+
+
+        CustomRecyclerAdapter mAdapter = new CustomRecyclerAdapter(array);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+}
